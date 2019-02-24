035af51401880cc6b14e8dcbc93090df6699806f
Sarah Mandi <snm62@georgetown.edu>
2019-02-24 14:30:29 -0500

    issue get request

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -21,8 +21,13 @@ while True:
 
 # see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
 # TODO: assemble the request url to get daily data for the given stock symbol...
-
-# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
+for s in symbols:
+    request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + s + "&apikey={api_key}"
+    # TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
+    response = requests.get(request_url)
+    #print("RESPONSE STATUS: " + str(response.status_code))
+    parsed_response = json.loads(response.text)
+    #print(parsed_response)
 
 # TODO: further parse the JSON response...
 
@@ -36,15 +41,16 @@ latest_price_usd = "$100,000.00"
 # TODO: write response data to a CSV file
 
 # TODO: further revise the example outputs below to reflect real information
-print("-----------------")
-print(f"STOCK SYMBOL: {symbols}")
-print("RUN AT: 11:52pm on June 5th, 2018")
-print("-----------------")
-print("LATEST DAY OF AVAILABLE DATA: June 4th, 2018")
-print(f"LATEST DAILY CLOSING PRICE: {latest_price_usd}")
-print("RECENT AVERAGE HIGH CLOSING PRICE: $101,000.00")
-print("RECENT AVERAGE LOW CLOSING PRICE: $99,000.00")
-print("-----------------")
-print("RECOMMENDATION: Buy!")
-print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
-print("-----------------")
+#for s in symbols:
+#    print("-----------------")
+#    print(f"STOCK SYMBOL: {s}")
+#    print("RUN AT: 11:52pm on June 5th, 2018")
+#    print("-----------------")
+#    print("LATEST DAY OF AVAILABLE DATA: June 4th, 2018")
+#    print(f"LATEST DAILY CLOSING PRICE: {latest_price_usd}")
+#    print("RECENT AVERAGE HIGH CLOSING PRICE: $101,000.00")
+#    print("RECENT AVERAGE LOW CLOSING PRICE: $99,000.00")
+#    print("-----------------")
+#    print("RECOMMENDATION: Buy!")
+#    print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
+#    print("------------------------------------------------------------------------------------------------")
