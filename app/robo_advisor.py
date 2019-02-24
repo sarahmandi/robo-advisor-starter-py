d9371e8c40991a3f54c7c3fab78459dad4343789
Sarah Mandi <snm62@georgetown.edu>
2019-02-24 15:21:02 -0500

    writing csv and recent high/low

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import json
+import csv
 import os
 import requests
 import datetime
@@ -8,7 +9,7 @@ now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
 
 def to_usd(my_price):
-    return "${0:,2f}".format(my_price)
+    return "${0:,.2f}".format(my_price)
 
 # see: https://www.alphavantage.co/support/#api-key
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
@@ -43,10 +44,19 @@ for s in symbols:
     latest_day = days[0] #> '2019-02-19'
     latest_closing = tsd[latest_day]["4. close"]
     print(latest_closing)
-    #recent_high = max(parsed_response["Time Series (Daily)"], key=lambda ev: ev["high"])
-    #print(recent_high)
-    #recent_low = 
 
+#with help from https://github.com/s2t2/robo-advisor-screencast/blob/master/app/robo_advisor.py
+high_prices = []
+low_prices = []
+
+for date in days:
+    high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
+    high_prices.append(float(high_price))
+    low_prices.append(float(low_price))
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
@@ -57,17 +67,41 @@ last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
 # TODO: write response data to a CSV file
 
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader() # uses fieldnames set above
+    for date in days:
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
+        })
+
+
+
 # TODO: further revise the example outputs below to reflect real information
-#for s in symbols:
-#    print("-----------------")
-#    print(f"STOCK SYMBOL: {s}")
-#    print("RUN AT: " +str(now))
-#    print("-----------------")
-#    print("LATEST DAY OF AVAILABLE DATA:{last_refresed}")
-#    print(f"LATEST DAILY CLOSING PRICE: {to_usd(float(latest_closing))}")
-#    print("RECENT AVERAGE HIGH CLOSING PRICE: $101,000.00")
-#    print("RECENT AVERAGE LOW CLOSING PRICE: $99,000.00")
-#    print("-----------------")
-#    print("RECOMMENDATION: Buy!")
-#    print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
-#    print("------------------------------------------------------------------------------------------------")
+for s in symbols:
+    print("-----------------")
+    print(f"STOCK SYMBOL: {s}")
+    print("RUN AT: " +str(now))
+    print("-----------------")
+    print("LATEST DAY OF AVAILABLE DATA:{last_refresed}")
+    print(f"LATEST CLOSE: {to_usd(float(latest_closing))}")
+    print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+    print(f"RECENT LOW: {to_usd(float(recent_low))}")
+    print("-----------------")
+    print("RECOMMENDATION: Buy!")
+    print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
+    print("-----------------")
+    print(f"WRITING DATA TO CSV: {csv_file_path}")
+    print("-----------------")
+    print("HAPPY INVESTING!")
+    print("------------------------------------------------------------------------------------------------")
